e05615dd90c79d0fdf32ba78990b9c349df3a4dc
haloninjamonkey <haloninjamonkey@gmail.com>
2019-04-30 21:59:42 -0600

    adds number guess game

--- Program.cs
+++ Program.cs
@@ -7,10 +7,11 @@ namespace hwapp
         static void Main(string[] args)
         {
             Console.Clear();
+            System.Console.WriteLine("Hello Patrick");
             // Console.WriteLine("Hello user, What is your name?");
             // string name = Console.ReadLine().ToLower();
-            Console.WriteLine("How old are you?");
-            string age = Console.ReadLine();
+            // Console.WriteLine("How old are you?");
+            // string age = Console.ReadLine();
             // Console.WriteLine("Hello, " + name + "." + " It sounds like " + name + " is " + age + " years old");
             // Console.WriteLine($"Hello, {name}. It sounds like {name} is {age} years old");
             // Console.WriteLine("Hello, {0}. It sounds like {0} is {1} years old", name, age);
@@ -50,25 +51,68 @@ namespace hwapp
 
             // }
 
-            int intAge = 0;
+            // int intAge = 0;
 
-            if (Int32.TryParse(age, out intAge))
-            {
-                if (intAge > 20) 
-                {
-                    System.Console.WriteLine("Cool, welcome to the club");
-                }
-                else
-                {
-                System.Console.WriteLine("Go back to school, kid");
-                }
+            // if (Int32.TryParse(age, out intAge))
+            // {
+            //     if (intAge > 20) 
+            //     {
+            //         System.Console.WriteLine("Cool, welcome to the club");
+            //     }
+            //     else
+            //     {
+            //     System.Console.WriteLine("Go back to school, kid");
+            //     }
                 
-            }
-            else
-            {
-                System.Console.WriteLine("Invalid input");
-            }
+            // }
+            // else
+            // {
+            //     System.Console.WriteLine("Invalid input");
+            // }
 
+            // for (int i = 0; i < 100; i++) {
+            //     // int num = 0;
+            //     // num += i;
+            //     if(i == 3){
+            //         System.Console.WriteLine("i is equal to 3");
+            //         continue;
+            //     }
+            //     System.Console.WriteLine("i is not 3, it is " + i);
+            //     if (i == 11){
+            //         break;
+            //     }
+            // }
+            // int i = 0;
+            // while(i < 100){
+            //     System.Console.Write('.');
+            //     i++;
+            // }
+            Random num = new Random();
+            int setNumber = num.Next(1,101);
+            // System.Console.WriteLine(setNumber);
+            for(int i = 5; i > 0; i--){
+                System.Console.WriteLine("Guess a number between 1 and 100");
+                string userGuess = Console.ReadLine();
+                int intGuess = 0;                
+                if (Int32.TryParse(userGuess, out intGuess)){
+                    if (intGuess != setNumber){
+                        if(i > 1 && intGuess > setNumber){
+                            System.Console.WriteLine("Guess lower, you have {0} tries left", (i-1));
+                        } else if (i > 1 && intGuess < setNumber) {
+                            System.Console.WriteLine("Guess higher, you have {0} tries left", (i-1));
+                        } else {
+                            System.Console.WriteLine("You have no more guesses, you did not guess correctly. The number is {0}.", setNumber);
+                        }
+                    } else if (intGuess == setNumber){
+                        System.Console.WriteLine("You win!");
+                        break;
+                    }
+                } else {
+                    System.Console.WriteLine("Integers only, please");
+                }
+            }
+                    
+                
             System.Console.WriteLine("Goodbye");
         }
     }
